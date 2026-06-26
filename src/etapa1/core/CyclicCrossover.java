89fc520485e23329fd7e7043c447fbad792e8ac5
c0piloto <moisesmlk123@gmail.com>
2026-06-26 18:24:59 -0300

    Fixed small logical error on CyclicCrossover. Stop condition check was a little inverted. Also noted future change that could be made in the form of randomly selecting start of the cycle although selecting index 0 isn't wrong.

--- src/etapa1/core/CyclicCrossover.java
+++ src/etapa1/core/CyclicCrossover.java
@@ -17,12 +17,20 @@ public class CyclicCrossover implements CrossoverStrategy {
     Arrays.fill(child2, -1);
 
     int currentIndex = 0;
+
+    // Iniciando com o primeiro valor, é válido mas pode ser uma melhoria
+    // escolher aleatóriamente, introduz mais variação
     int startingValue = parent1[0];
+
     while (true) {
       child1[currentIndex] = parent1[currentIndex];
       child2[currentIndex] = parent2[currentIndex];
 
       int valueFromParent2 = parent2[currentIndex];
+
+      if (valueFromParent2 == startingValue) {
+        break;
+      }
 
       int nextIndex = -1;
       for (int i = 0; i < 10; i++) {
@@ -32,16 +40,15 @@ public class CyclicCrossover implements CrossoverStrategy {
         }
       }
       currentIndex = nextIndex;
-      if (parent1[currentIndex] == startingValue) {
-        break;
-      }
     }
+
     for (int i = 0; i < 10; i++) {
       if (child1[i] == -1) {
         child1[i] = parent2[i];
         child2[i] = parent1[i];
       }
     }
+
     return new Individual[] {
         new Individual(child1),
         new Individual(child2)
